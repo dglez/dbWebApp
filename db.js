06f4ce6eed0e711ac86c520048ebbba297e3772d
fredlo2010 <alfredlopezf@hotmail.com>
2016-11-24 12:38:46 -0500

    Fixed the problems we are having for the database

--- db.js
+++ db.js
@@ -5,30 +5,30 @@ var fs = require('fs');
 var content = fs.readFileSync('./dbCredentials.json');
 var credentials = JSON.parse(content);
 
-// creating connection 
+// creating connection
 var connection = mysql.createConnection(credentials);
 
-module.exports.connect = function () {
-    connection.connect(function (err) {
 
-        if (err) {
-            console.error("error connection" + err.stack);
-            return;
-        }
-        console.log("I was able to connnect" + connection.threadId);
-    });
+module.exports = {
+    connect: function () {
+        connection.connect(function (err) {
+
+            if (err) {
+                console.error("error connection" + err.stack);
+                return;
+            }
+            console.log("I was able to connnect" + connection.threadId);
+        });
+    },
+
+    query: function (sql, callback) {
+        connection.query(sql, function (err, rows, fields) {
+
+            if (err) {
+                return callback(err, {}, {});
+            }
+            //connection.end();
+            return callback(err, rows, fields);
+        });
+    }
 };
-
-module.exports.query = function (sql, callback) {
-    connection.query(sql, function (err, rows, fields) {
-
-        if (err) {
-            return callback(err, {}, {});
-        }
-        return callback(err, rows, fields);
-    });
-};
-
-
-
-
